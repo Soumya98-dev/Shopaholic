3a407b928daf36f2b7efe6d7c4f1e62f7729ad48
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2022-08-07 04:12:29 +0530

    Dynamically add of list items in the grocery list

--- script.js
+++ script.js
@@ -17,13 +17,41 @@
 //*** APPROACH: 2 (WITH EVENT BUBBLING)
 
 //** ACCESSING THE <ul> tag
-let ul = document.querySelector('ul');
+let ul = document.querySelector("ul");
 // console.log(ul);
-ul.addEventListener('click', removeLI);
+ul.addEventListener("click", removeLI);
+
 function removeLI(e) {
-  let target = e.target;
-  if (target.className == 'delete') {
-    // console.log(target.parentElement);
-    target.parentElement.remove();
-  }
+    let target = e.target;
+    if (target.className === "delete") {
+        // console.log(target.parentElement);
+        target.parentElement.remove();
+    }
 }
+
+//*** ACCESSING THE FORM input: middle-context
+let inputMiddleContext = document.querySelector(".middle-context input");
+// console.log(inputMiddleContext);
+
+//*** ACCESSING THE FORM button: middle-context
+let buttonMiddleContext = document.querySelector(".middle-context button");
+// console.log(buttonMiddleContext);
+buttonMiddleContext.addEventListener("click", (event) => {
+    let text = inputMiddleContext.value;
+    //*** CREATING A NEW <li> TAG
+    const li = document.createElement('li');
+    document.querySelector("ul").appendChild(li);
+    //*** CREATING A NEW <span> TAG INSIDE THE <li>
+    const newSpan = document.createElement('span');
+    li.appendChild(newSpan);
+    newSpan.classList.add('item');
+    newSpan.innerHTML = text;
+    //*** CREATING A NEW BUTTON AFTER THE <span> INSIDE THE NEW <li>
+    const newBtn = document.createElement('span');
+    li.appendChild(newBtn);
+    newBtn.classList.add('delete');
+    newBtn.innerHTML = 'delete';
+
+    inputMiddleContext.value = '';
+    event.preventDefault();
+});
